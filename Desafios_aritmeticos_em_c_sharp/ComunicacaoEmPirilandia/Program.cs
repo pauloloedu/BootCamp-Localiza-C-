b107e6236c63582f185c39c8faea30b6bb37c1d8
pauloloedu <pauloeduardorvieira@gmail.com>
2021-04-14 08:43:48 -0300

    Atualizando ambiente
    ok

---
+++ Desafios_aritmeticos_em_c_sharp/ComunicacaoEmPirilandia/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ComunicacaoEmPirilandia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n;
+            bool converte;
+            Int64 enumero;
+            int cont = 0;
+            int cont2 = 0;
+
+            n = Console.ReadLine();
+            converte = Int64.TryParse(n, out enumero);
+
+            if (converte == false || enumero <= 0 || enumero >= 9999999999) return;
+
+            else
+            {
+                char[] vetor1 = new char[n.Length];
+                char[] vetor2 = new char[n.Length];
+
+                foreach (char a in n)
+                {
+                    vetor1[cont] = a;
+                    cont += 1;
+                }
+
+                for (int i = n.Length -1; i >= 0; i--)
+                {
+                    vetor2[cont2] = vetor1[i];
+                    cont2 += 1;
+                }
+
+                foreach (char b in vetor2)
+                {
+                    Console.Write(b);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Desafios_aritmeticos_em_c_sharp/TempoDeUmEvento/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace TempoDeUmEvento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int diaInicio, diaTermino, horaMomentoInicio, minutoMomentoInicio, segundoMomentoInicio,
+                horaMomentoTermino, minutoMomentoTermino, segundoMomentoTermino;
+
+            bool converteDiaInicio, converteDiaTermino, converteHoraMomentoInicio,
+                 converteHoraMomentoTermino, converteMinutoMomentoInicio, converteSegundoMomentoInicio,
+                 converteMinutoMomentoTermino, converteSegundoMomentoTermino;
+
+            try
+            {
+                string[] dadosInicio = Console.ReadLine().Split(" ");
+                converteDiaInicio = Int32.TryParse(dadosInicio[1], out diaInicio);
+
+                string[] dadosMomentoInicio = Console.ReadLine().Split(" : ");
+                converteHoraMomentoInicio = Int32.TryParse(dadosMomentoInicio[0], out horaMomentoInicio);
+                converteMinutoMomentoInicio = Int32.TryParse(dadosMomentoInicio[1], out minutoMomentoInicio);
+                converteSegundoMomentoInicio = Int32.TryParse(dadosMomentoInicio[2], out segundoMomentoInicio);
+
+                string[] dadosTermino = Console.ReadLine().Split(" ");
+                converteDiaTermino = Int32.TryParse(dadosTermino[1], out diaTermino);
+
+                string[] dadosMomentoTermino = Console.ReadLine().Split(" : ");
+                converteHoraMomentoTermino = Int32.TryParse(dadosMomentoTermino[0], out horaMomentoTermino);
+                converteMinutoMomentoTermino = Int32.TryParse(dadosMomentoTermino[1], out minutoMomentoTermino);
+                converteSegundoMomentoTermino = Int32.TryParse(dadosMomentoTermino[2], out segundoMomentoTermino);
+            }
+            catch
+            {
+                return;
+            }
+
+            if (converteDiaInicio == false ||
+                converteDiaTermino == false ||
+                converteHoraMomentoInicio == false ||
+                converteHoraMomentoTermino == false ||
+                converteMinutoMomentoInicio == false ||
+                converteSegundoMomentoInicio == false ||
+                converteMinutoMomentoTermino == false ||
+                converteSegundoMomentoTermino == false) return;
+
+            var inicio = (diaInicio - 1) * 86400 + horaMomentoInicio * 3600 + minutoMomentoInicio * 60 + segundoMomentoInicio;
+            
+            var fim = (diaTermino - 1) * 86400 + horaMomentoTermino * 3600 + minutoMomentoTermino * 60 + segundoMomentoTermino;
+            
+            var duracao = fim - inicio;
+
+            int W = duracao / 86400;
+            int resto = duracao % 86400;
+
+            int X = resto / 3600;
+            resto = resto % 3600;
+
+            int Y = resto / 60;
+
+            int Z = resto % 60;
+
+            Console.WriteLine("{0} dia(s)", W);
+            Console.WriteLine("{0} hora(s)", X);
+            Console.WriteLine("{0} minuto(s)", Y);
+            Console.WriteLine("{0} segundo(s)", Z);
+
+            Console.ReadLine();
+        }
+    }
+}
